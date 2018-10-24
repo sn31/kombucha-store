ebe3eed9c48957e9690c34433bad67770c781201
Skye Nguyen and Victoria Voh <skye@>
2018-10-24 10:09:06 -0700

    Add Details page.

--- src/app/flavors/flavors.component.ts
+++ src/app/flavors/flavors.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Flavor } from '../models/flavors-model';
 
 @Component({
   selector: 'app-flavors',
@@ -7,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FlavorsComponent implements OnInit {
 
+  flavor: Flavor = {
+    name: 'chamomile',
+    brand: 'SkyeTown',
+    sugarContent: 80,
+    volume: 24
+  };
+
+
   constructor() { }
 
   ngOnInit() {
   }
 
-  flavor = 'Chamomile';
+  
 }
---
+++ src/app/models/flavors-model.ts
@@ -0,0 +1,5 @@
+export class Flavor{
+    public volume: number = 24;
+  
+    constructor(public name: string, public brand: string, public sugarContent: number) {};
+}
